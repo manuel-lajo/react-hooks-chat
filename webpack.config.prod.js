f4c0d6c73809578a06ac0d8abf8f100cea39d050
Manuel Lajo <mlajo.salazar@gmail.com>
2021-01-18 00:59:24 -0500

    complete tooling & base project

---
+++ webpack.config.prod.js
@@ -0,0 +1,10 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
+
+module.exports = merge(commonConfig, {
+  mode: 'production',
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+});
